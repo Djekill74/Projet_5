cade248b223e4335216d721205f14f259a1d9e7a
Djekill74 <jerome.delagneau@orange.fr>
2020-11-16 09:00:00 +0100

    vitefait

--- front/js/finish.js
+++ front/js/finish.js
@@ -1,4 +1,7 @@
 const listOrder = JSON.parse(localStorage.getItem('order'));
 console.log(listOrder);
-console.log("orderid:"+listOrder.orderId)
-console.log("nom : "+listOrder.contact.firstName)
+
+const nameRepeat=document.getElementById("name-repeat");
+nameRepeat.innerHTML=listOrder.contact.firstName;
+const numberID=document.getElementById("recapitulatif");
+numberID.innerHTML=listOrder.orderId;
